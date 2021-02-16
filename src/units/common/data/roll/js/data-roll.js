e26fbdbb0ee7f4ffcda649d1e7da220abd1f0b2e
Marcus Downing <marcus.downing@gmail.com>
2021-02-16 00:11:24 +0000

    Edit and save data

---
+++ src/units/common/data/roll/js/data-roll.js
@@ -0,0 +1,31 @@
+
+// Roll dice
+
+function rollDice(number, die, bonus) {
+  console.log("Roll "+number+"d"+die+"+"+bonus);
+}
+
+for (var field of document.getElementsByClassName("field")) {
+  for (var icon of field.getElementsByClassName("icon_bonus")) {
+    icon.addEventListener('click', function (event) {
+
+      if (icon.classList.includes("icon_bonus")) {
+        var input = event.target.closest('.field').getElementsByTagName('input')[0];
+        var bonus = input.value;
+      
+        if (bonus !== null && bonus !== "") {
+          bonus = parseInt(bonus);
+      
+          if (!isNaN(bonus)) {
+            rollDice(1, 20, bonus);
+          }
+        }
+      } else if (icon.classList.includes("icon_damage")) {
+        // ... damage dice ...
+      }
+
+    });
+  }
+
+
+}
